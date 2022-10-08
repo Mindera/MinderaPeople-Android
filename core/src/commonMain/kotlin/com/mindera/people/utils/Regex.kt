933d82e149c89fa51246547dc7c450646d57cbae
Magnum Rocha <magnum.barbosa.rocha@gmail.com>
2022-10-08 17:32:19 +0100

    code improvements

--- core/src/commonMain/kotlin/com/mindera/people/utils/Regex.kt
+++ core/src/commonMain/kotlin/com/mindera/people/utils/Regex.kt
@@ -1,10 +1,12 @@
 package com.mindera.people.utils
 
 val emailAddressRegex: Regex
-    get() = Regex(pattern = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-            "\\@" +
-            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-            "(" +
-            "\\." +
-            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-            ")+")
+    get() = Regex(pattern = buildString {
+        append("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}")
+        append("\\@")
+        append("[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}")
+        append("(")
+        append("\\.")
+        append("[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}")
+        append(")+")
+    })
