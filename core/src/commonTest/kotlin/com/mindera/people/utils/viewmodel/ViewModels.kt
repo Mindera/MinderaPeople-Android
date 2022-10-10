50fba88dd55ea91a9879cc143268865568f3b3af
Magnum Rocha <magnum.barbosa.rocha@gmail.com>
2022-10-10 20:19:22 +0100

    improve unit tests

--- core/src/commonTest/kotlin/com/mindera/people/utils/viewmodel/ViewModels.kt
+++ core/src/commonTest/kotlin/com/mindera/people/utils/viewmodel/ViewModels.kt
@@ -3,8 +3,10 @@ package com.mindera.people.utils.viewmodel
 import com.mindera.people.utils.StateViewModel
 import com.mindera.people.utils.viewmodel.TestViewModel.Action
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.milliseconds
 
 data class CombinedState(val items: List<Int>)
@@ -13,24 +15,27 @@ class TestViewModel: StateViewModel<Action, CombinedState>(
     initialState = CombinedState(items = emptyList())
 ) {
     override val mainDispatcher: CoroutineDispatcher get() = StandardTestDispatcher()
+    override val ioDispatcher: CoroutineDispatcher get() = StandardTestDispatcher()
 
     fun action1() { enqueueAction(Action.Action1) }
     fun action2() { enqueueAction(Action.Action2) }
     fun action3() { enqueueAction(Action.Action3) }
 
     override suspend fun processAction(action: Action, latestState: CombinedState): CombinedState =
-        when (action) {
-            is Action.Action1 -> {
-                delay(100.milliseconds)
-                CombinedState(items = listOf(1))
-            }
-            is Action.Action2 -> {
-                delay(100.milliseconds)
-                latestState.copy(items = latestState.items + listOf(2))
-            }
-            is Action.Action3 -> {
-                delay(100.milliseconds)
-                latestState.copy(items = latestState.items + listOf(3))
+        withContext(Dispatchers.Default) {
+            return@withContext when (action) {
+                is Action.Action1 -> {
+                    delay(100.milliseconds)
+                    CombinedState(items = listOf(1))
+                }
+                is Action.Action2 -> {
+                    delay(200.milliseconds)
+                    latestState.copy(items = latestState.items + listOf(2))
+                }
+                is Action.Action3 -> {
+                    delay(100.milliseconds)
+                    latestState.copy(items = latestState.items + listOf(3))
+                }
             }
         }
 
