3cca658cf5377bb7d6f5905110d327837d0f9cdb
Magnum Rocha <magnum.barbosa.rocha@gmail.com>
2022-11-26 19:38:02 +0000

    add current user to `HomeKt` wrapper

--- core/src/iosMain/kotlin/com/mindera/people/wrappers/HomeKt.kt
+++ core/src/iosMain/kotlin/com/mindera/people/wrappers/HomeKt.kt
@@ -1,17 +1,20 @@
 package com.mindera.people.wrappers
 
+import com.mindera.people.auth.GetAuthenticatedUserUseCase
 import com.mindera.people.auth.SignInUseCase
 import com.mindera.people.auth.User
 import com.mindera.people.data.Error
 import com.mindera.people.data.toError
-import com.mindera.people.utils.MainScope
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 object HomeKt : KoinComponent {
-    private val mainScope = MainScope()
+    // private val mainScope = MainScope()
 
+    private val getAuthenticatedUserUseCase: GetAuthenticatedUserUseCase by inject()
     private val signInUseCase: SignInUseCase by inject()
+
+    val user: User? get() = getAuthenticatedUserUseCase()
 
     fun setUser(user: User, callback: (Result) -> Unit) {
         signInUseCase(user).fold(
